fe0a463b6f6c359f3211b5a2da6a09eb213c60d2
Morteza Ibrahimi <morteza@>
2022-09-29 18:39:57 -0400

    A simple experiment objects for running multiple trials of an egent/environment loop.

--- examples/experiments/test_experiment.py
+++ examples/experiments/test_experiment.py
@@ -1,27 +1,43 @@
+# Copyright 2022 Morteza Ibrahimi. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+"""A simple test experiment with random environment  and agent."""
+
+
 import numpy as np
 
 from rlba.environment_loop import EnvironmentLoop
-from rlba.environments import RandomEnvironment 
+from rlba.environments import RandomEnvironment
 from rlba.agents import RandomAgent
 from rlba.types import ArraySpec, DiscreteArraySpec
 
 if __name__ == "__main__":
 
-  n_action = 3
-  action_dtype = np.int32
-  obs_shape = (5, 7)
-  obs_dtype = np.float32
+    n_action = 3
+    action_dtype = np.int32
+    obs_shape = (5, 7)
+    obs_dtype = np.float32
 
-  observation_spec = ArraySpec(
-      shape=obs_shape,
-      dtype=obs_dtype)
-  action_spec = DiscreteArraySpec(n_action, dtype=action_dtype)
-  env = RandomEnvironment(
-    action_spec=action_spec,
-    observation_spec=observation_spec,
-    seed=0,
-  )
-  agent = RandomAgent(action_spec, observation_spec, 0)
+    observation_spec = ArraySpec(shape=obs_shape, dtype=obs_dtype)
+    action_spec = DiscreteArraySpec(n_action, dtype=action_dtype)
+    env = RandomEnvironment(
+        action_spec=action_spec,
+        observation_spec=observation_spec,
+        seed=0,
+    )
+    agent = RandomAgent(action_spec, observation_spec, 0)
 
-  loop = EnvironmentLoop(env, agent)
-  loop.run(100)
+    loop = EnvironmentLoop(env, agent)
+    loop.run(100)
--- rlba/utils/experiment_utils.py
+++ rlba/utils/experiment_utils.py
@@ -12,17 +12,69 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Utility definitions for RLBA experiments."""
+"""Utility for running experiments."""
 
 from typing import Optional
 
-from rlba.utils import loggers
+
+from typing import Callable, Iterator, Tuple
+import pandas as pd
+from rlba.agent import Agent
+from rlba.environment import Environment
+from rlba.environment_loop import EnvironmentLoop
+from rlba.utils.loggers import Logger, make_default_logger, TerminalLogger
+from rlba.utils.observers.base import EnvLoopObserver
+import time
+
+
+class Experiment(object):
+    """A simple experiment object that can run agent/env loop for multiple trials."""
+
+    def __init__(
+        self,
+        loop_factory: Callable[[int], Tuple[EnvironmentLoop, Environment, Agent]],
+        logger: Logger = TerminalLogger("experiment", print_fn=print),
+    ):
+        """
+        Args:
+          loop_factory: a callable that get an integer seed and returns
+              environment loop plus environment and agent.
+          logger: a logger to log the loop stats.
+        """
+
+        self._loop_factory = loop_factory
+        self._logger = logger
+        self._result_df = None
+
+    def run(self, seeds: Iterator[int], time_horizon: int) -> None:
+        """Runs an agent on an environment.
+        Args:
+          time_horizon: Number of steps in each trial.
+          seeds: iterator of seeds value. Each seed corresponds to one trial.
+        Return:
+            a pandas dataframe containing the results of the experiment.
+        """
+
+        results = []
+        for sidx, seed in enumerate(seeds):
+            loop, _, _ = self._loop_factory(seed)
+            start_time = time.time()
+            res = loop.run(time_horizon)
+            res = pd.DataFrame(res)
+            res["trial_id"] = sidx
+            res["seed"] = seed
+            results.append(res)
+            self._logger.write(
+                {"trial ": sidx, "exec time(s)": time.time() - start_time}
+            )
+        result_df = pd.concat(results)
+        return result_df
 
 
 def make_experiment_logger(
     label: str, steps_key: Optional[str] = None, task_instance: int = 0
-) -> loggers.Logger:
+) -> Logger:
     del task_instance
     if steps_key is None:
         steps_key = f"{label}_steps"
-    return loggers.make_default_logger(label=label, steps_key=steps_key)
+    return make_default_logger(label=label, steps_key=steps_key)
